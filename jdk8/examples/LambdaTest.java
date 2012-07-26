dddab25c00d31401c849ca28e48116f5780b5ea7
Anthony Bargnesi <abargnesi@gmail.com>
2012-07-25 20:06:48 -0700

    added java 8 forEach and filter examples

--- jdk8/examples/LambdaTest.java
+++ jdk8/examples/LambdaTest.java
@@ -2,17 +2,32 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterable;
+import static java.lang.System.out.println;
+
+// juicy bits
+import java.util.functions.*;
 
 public class LambdaTest {
     private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
     public static void main(final String[] args) throws ParseException {
         final List<Person> people = new ArrayList<Person>();
-        people.add(new Person("Tony", df.parse("1974-03-03"), 30));
-        people.add(new Person("William", df.parse("1984-03-03"), 24));
-        people.add(new Person("Jordan", df.parse("1984-07-03"), 42));
-        people.add(new Person("Nick", df.parse("1991-04-03"), 19));
-        people.add(new Person("James", df.parse("1980-09-03"), 52));
+        people.add(new Person("Tony", 30, 1));
+        people.add(new Person("William", 40, 2));
+        people.add(new Person("Jordan", 24, 1));
+        people.add(new Person("Nick", 34, 2));
+        people.add(new Person("James", 28, 0));
+
+        // forEach with Lambda
+        people.forEach(p -> {println(p.getName())});
+
+        // filter
+        Predicate<Integer> encumbered = p -> p.getNumChildren() > 0;
+        Iterable<People> parents = people.filter(encumbered)
+        parents.forEach(p -> {println(p.getName())};
+
+        // filter/map/reduce
     }
 }
 
--- jdk8/examples/Person.java
+++ jdk8/examples/Person.java
@@ -5,37 +5,37 @@ import java.util.Date;
  */
 public class Person {
     private String name;
-    private Date birth;
     private int age;
+    private int numChildren;
 
-    public Person(final String name, final Date birth, final int age) {
+    public Person(final String name, final int age, final int numChildren) {
         if (name == null || "".equals(name)) {
             throw new IllegalArgumentException("name cannot be null");
-        }
-
-        if (birth == null) {
-            throw new IllegalArgumentException("birth cannot be null");
         }
 
         if (age <= 0) {
             throw new IllegalArgumentException("age cannot be less or equal to zero");
         }
 
+        if (numChildren < 0) {
+            throw new IllegalArgumentException("cannot have negative children, you're stuck with them");
+        }
+
         this.name = name;
-        this.birth = birth;
         this.age = age;
+        this.numChildren = numChildren;
     }
 
     public String getName() {
         return name;
     }
 
-    public Date getBirth() {
-        return birth;
-    }
-
     public int getAge() {
         return age;
+    }
+
+    public int getNumChildren() {
+        return numChildren;
     }
 }
 
